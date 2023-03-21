dfe5ca4ac549af1624fc8f85d94a7584e757253d
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-03-20 18:29:14 -0700

    Update Navigation.js

--- Unicoins/my-app/src/components/Navigation.js
+++ Unicoins/my-app/src/components/Navigation.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 
-function Navigation() {
-  return (
-    <Navbar bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="/">UNCollaboration</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/tasks">Tasks</Nav.Link>
-          <Nav.Link href="/projects">Projects</Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
-  );
-}
+const Navigation = () => (
+  <Navbar bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand href="/">UNCollaboration</Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/tasks">Tasks</Nav.Link>
+        <Nav.Link href="/projects">Projects</Nav.Link>
+      </Nav>
+    </Container>
+  </Navbar>
+);
 
 export default Navigation;
