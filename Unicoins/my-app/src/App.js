7f711a98dbd7345a8ee1211a1eadb0e74456edd4
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-03-20 18:39:09 -0700

    Update App.js

--- Unicoins/my-app/src/App.js
+++ Unicoins/my-app/src/App.js
@@ -3,17 +3,84 @@ import Navbar from './components/Navbar';
 import Tasks from './components/Tasks';
 import Projects from './components/Projects';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import Web3 from 'web3';
+import UNCollaborationABI from './UNCollaborationABI.json';
+import UNBadgeABI from './UNBadgeABI.json';
 
-function App() {
+const UNCollaborationAddress = 'REPLACE_WITH_UNCOLLABORATION_CONTRACT_ADDRESS';
+const UNBadgeAddress = 'REPLACE_WITH_UNBADGE_CONTRACT_ADDRESS';
+
+const App = () => {
+  const [web3, setWeb3] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [UNCollaborationContract, setUNCollaborationContract] = useState(null);
+  const [UNBadgeContract, setUNBadgeContract] = useState(null);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const initWeb3 = async () => {
+      if (window.ethereum) {
+        const web3Instance = new Web3(window.ethereum);
+        setWeb3(web3Instance);
+      } else {
+        console.log('Please install MetaMask!');
+      }
+    };
+
+    initWeb3();
+  }, []);
+
+  useEffect(() => {
+    const initContracts = async () => {
+      if (web3) {
+        const accounts = await web3.eth.getAccounts();
+        setAccount(accounts[0]);
+
+        const UNCollaboration = new web3.eth.Contract(UNCollaborationABI, UNCollaborationAddress);
+        setUNCollaborationContract(UNCollaboration);
+
+        const UNBadge = new web3.eth.Contract(UNBadgeABI, UNBadgeAddress);
+        setUNBadgeContract(UNBadge);
+      }
+    };
+
+    initContracts();
+  }, [web3]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      if (UNCollaborationContract) {
+        const taskCount = await UNCollaborationContract.methods.getTaskCount().call();
+        const fetchedTasks = [];
+
+        for (let i = 0; i < taskCount; i++) {
+          const task = await UNCollaborationContract.methods.tasks(i).call();
+          fetchedTasks.push(task);
+        }
+
+        setTasks(fetchedTasks);
+      }
+    };
+
+    fetchTasks();
+  }, [UNCollaborationContract]);
+
+  // Add other functions like addTask, completeTask, awardBadge, etc., using the smart contract's methods.
+
   return (
-    <Web3Provider>
-      <div className="App">
-        <Navbar />
-        <Tasks />
-        <Projects />
-      </div>
-    </Web3Provider>
+    <div>
+      <h1>UNCollaboration Platform</h1>
+      <h2>Tasks</h2>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task.taskDescription} - Reward: {task.reward} UNCs - Status: {task.completed ? 'Completed' : 'Incomplete'}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-}
+};
 
 export default App;
