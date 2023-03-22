d3dac615f3dcd2da47820392b3327f9517526445
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-03-21 17:45:53 -0700

    Update Home.js

--- Unicoins/my-app/src/components/Home.js
+++ Unicoins/my-app/src/components/Home.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import './Home.css';
 
 const Home = () => (
   <Container>
-    <h1>Welcome to UNCollaboration</h1>
-    <p>
-      This is a platform to collaborate on projects, participate in tasks, and earn rewards.
-    </p>
+    <Row className="justify-content-center align-items-center">
+      <Col md={8} className="home-content">
+        <h1 className="display-4">Welcome to UNCollaboration</h1>
+        <p className="lead">
+          UNCollaboration is a platform that brings together volunteers and project managers to collaborate on projects, participate in tasks, and earn rewards.
+        </p>
+        <hr className="my-4" />
+        <p>
+          Join our growing community and contribute to meaningful projects, learn new skills, and make a difference.
+        </p>
+      </Col>
+    </Row>
   </Container>
 );
 
 export default Home;
+
