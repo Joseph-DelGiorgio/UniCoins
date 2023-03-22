830644f684f1bb7123863a8ab054951e8c4a3896
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-03-21 17:52:17 -0700

    Update Navigation.js

--- Unicoins/my-app/src/components/Navigation.js
+++ Unicoins/my-app/src/components/Navigation.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 
 const Navigation = () => (
-  <Navbar bg="dark" variant="dark">
+  <Navbar bg="dark" variant="dark" expand="lg">
     <Container>
       <Navbar.Brand href="/">UNCollaboration</Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link href="/">Home</Nav.Link>
-        <Nav.Link href="/tasks">Tasks</Nav.Link>
-        <Nav.Link href="/projects">Projects</Nav.Link>
-      </Nav>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/tasks">Tasks</Nav.Link>
+          <Nav.Link href="/projects">Projects</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Container>
   </Navbar>
 );
